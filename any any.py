8c7ac93eb4ba75da96902521af9cb0e9bb872687
Aakriti Yadav <aakritiyadav82@gmail.com>
2022-08-16 10:49:08 +0545

    any any

---
+++ any any.py
@@ -0,0 +1,12 @@
+#--> My name is Aakriti Yadav. 
+
+'''
+My name is Aakriti Yadav. 
+I live in Imadol.
+I completed my high school from V.S.Niketan school.
+'''
+
+name = "aakriti"    #here, name is variable which store/holds string type value i.e "aakriti"
+
+# add python srtring are always inside or double quotes:
+# for e4xample: "aakriti" or 'aakriti'
